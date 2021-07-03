56285b3e5621c376497a9dcfb766749487168033
Ale <freeale12@gmail.com>
2021-07-03 13:48:53 +0200

    fix typo

--- config/server.js
+++ config/server.js
@@ -1,6 +1,6 @@
 module.exports = ( { env } ) => ( {
-  host: env( 'HOST', '0.0.0.0' ),
-  port: env.int( 'PORT', 3000 ),
+  host: "${process.env.HOST || '0.0.0.0'}",
+  port: "${process.env.PORT || 3000}",
   admin: {
     auth: {
       secret: env( 'ADMIN_JWT_SECRET', '64b03cce75d9ff8a77a1e40fc1441b73' ),
